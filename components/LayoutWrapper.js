0d1973451ba621a9842a22e1d0a3a19f18dd2de8
Prajwxl <prajwify@gmail.com>
2023-03-28 21:55:48 +0530

    enlarge graidnet

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -59,7 +59,7 @@ function HeaderGradient() {
 function FooterGradient() {
   return (
     <div className="motion-safe:animate-rotate-colors pointer-events-none z-[-1] mx-auto max-w-6xl">
-      <div className="absolute inset-x-0 h-[200px] rounded-t-full bg-gradient-to-r from-amber-500 via-indigo-500 to-emerald-500 opacity-20 blur-3xl" />
+      <div className="absolute inset-x-0 h-[400px] rounded-t-full bg-gradient-to-r from-amber-500 via-indigo-500 to-emerald-500 opacity-20 blur-3xl" />
     </div>
   )
 }
