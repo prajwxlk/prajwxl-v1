4c872e974ea86f528f2d0cb288104fb56ff3fa7a
Prajwxl <prajwify@gmail.com>
2023-03-28 23:42:23 +0530

    few update

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,16 +1,16 @@
 const projectsData = [
   {
-    title: 'Internet is Awesome',
+    title: '📚 Internet is Awesome',
     description: `Collection of Courses from Top Universities.`,
     href: 'https://github.com/prajwxlk/ivyleague2-0',
   },
   {
-    title: 'Notion Database CMS',
+    title: '🗂️ Notion Database CMS',
     description: `Using Notion Database as CMS for your Website`,
     href: 'https://github.com/prajwxlk/NotionDatabase_CMS',
   },
   {
-    title: 'TicToe',
+    title: '🎲 TicToe',
     description: `It is the classic TicTacToe Game.`,
     href: 'https://play.google.com/store/apps/details?id=prajwal.kadbane.tictoe&hl=en_IE',
   },
