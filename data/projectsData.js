65f01b3b6f0fc7e332259d654a762d9324660160
Prajwal <73160418+prajwxlk@users.noreply.github.com>
2023-04-11 14:51:21 +0530

    added https

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,22 +2,22 @@ const projectsData = [
   {
     title: '⚡ Aretus : A Powerful ChatGPT Client',
     description: `ChatGPT Client for Power Users`,
-    href: 'aretus.app/',
+    href: 'https://aretus.app/',
   },
   {
     title: '📸 AI Image Generator',
     description: `LORA Fine Tuned SD Image Generator.`,
-    href: 'sd-image-generator.vercel.app/',
+    href: 'http://sd-image-generator.vercel.app/',
   },
   {
     title: '🗂️ Notion Database CMS',
     description: `Using Notion Database as CMS for your Website`,
-    href: 'github.com/prajwxlk/NotionDatabase_CMS',
+    href: 'https://github.com/prajwxlk/NotionDatabase_CMS',
   },
   {
     title: '🎲 TicToe',
     description: `It is the classic TicTacToe Game.`,
-    href: 'play.google.com/store/apps/details?id=prajwal.kadbane.tictoe&hl=en_IE',
+    href: 'https://play.google.com/store/apps/details?id=prajwal.kadbane.tictoe&hl=en_IE',
   },
 ]
 
